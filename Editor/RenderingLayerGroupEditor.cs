c9242c5b6bb33e57f988bf20156668ea6d284080
Symin <syminomega@outlook.com>
2024-09-06 09:55:32 +0800

    添加多物体编辑支持
    RenderLayerGroup可同时编辑多个物体

--- Editor/RenderingLayerGroupEditor.cs
+++ Editor/RenderingLayerGroupEditor.cs
@@ -4,26 +4,49 @@ using UnityEngine.Rendering;
 
 namespace SyminStudio.Rendering.Universal.Editor
 {
+    [CanEditMultipleObjects]
     [CustomEditor(typeof(RenderingLayerGroup))]
     public class RenderingLayerGroupEditor : UnityEditor.Editor
     {
-        private RenderingLayerGroup _renderingLayerGroup;
+        //private RenderingLayerGroup _renderingLayerGroup;
+
+        private SerializedProperty _renderingLayerMaskProp;
+
         //初始化
         private void OnEnable()
         {
-            _renderingLayerGroup = (RenderingLayerGroup)target;
+            //_renderingLayerGroup = (RenderingLayerGroup)target;
+            _renderingLayerMaskProp = serializedObject.FindProperty("renderingLayerMask");
         }
 
         //绘制 Inspector UI
         public override void OnInspectorGUI()
         {
+            // 更新 SerializedObject
+            serializedObject.Update();
+
             // 绘制默认的 Inspector UI
             // base.OnInspectorGUI();
-            //绘制自定义的 Inspector UI
+
+            // 绘制自定义的 Inspector UI
             EditorGUILayout.LabelField("Rendering Layer Mask");
             var layerNames = GraphicsSettings.currentRenderPipeline.renderingLayerMaskNames;
-            // 将layerNames作为枚举的选项
-            _renderingLayerGroup.RenderingLayerMask = (RenderingLayer)EditorGUILayout.MaskField((int)_renderingLayerGroup.RenderingLayerMask, layerNames);
+
+            // 处理多个对象的不同值
+            EditorGUI.showMixedValue = _renderingLayerMaskProp.hasMultipleDifferentValues;
+            EditorGUI.BeginChangeCheck();
+            var maskValue =
+                EditorGUILayout.MaskField("Rendering Layer Mask", _renderingLayerMaskProp.intValue, layerNames);
+            if (EditorGUI.EndChangeCheck())
+            {
+                _renderingLayerMaskProp.intValue = maskValue;
+            }
+
+            EditorGUI.showMixedValue = false;
+
+
+            // 应用属性修改
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
